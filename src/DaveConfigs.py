2a63bd904f3117b27dc19359aeba0333d3117da1
Ryan Coffee <coffeer76@gmail.com>
2021-01-22 21:25:51 -0800

    Finished Config map definitions

--- src/DaveConfigs.py
+++ src/DaveConfigs.py
@@ -11,9 +11,17 @@ class mappings:
     def __init__(self,shot = None):
         self.configdict = {}
         self.sepdict = {}
-        shotmap['s142294'] = ('config0','sepstate0')
+        setshotconfigs()
+        setshotsepstate()
+        shotmap['s142294'] = ('config0','sepstate0') 
         shotmap['s142300'] = ('config0','sepstate1')
         shotmap['s145384'] = ('config1','sepstate2')
+        shotmap['s145747'] = ('config1','sepstate3')
+        shotmap['s171472'] = ('config2','sepstate4')
+        shotmap['s171473'] = ('config2','sepstate4')
+        shotmap['s171477'] = ('config2','setstate5')
+        shotmap['s171495'] = ('config2','sepstate5')
+        shotmap['s176778'] = ('config3','sepstate6')
         if shot == None:
             return None
         return None
@@ -29,7 +37,7 @@ class mappings:
         return self
 
     def setshotconfigs(self):
-        #config0
+        #config0, page 1 & 2, shots 142294, 142300
         configdict['config0'] = {}
         cd = configdict['config0']
         cd[32] = (31,0)
@@ -76,7 +84,9 @@ class mappings:
         cols = [i for i in range(18,21)]
         setpos(cd,chans,rows,cols)
 
-        #config1
+        #config1, page 3 & 4, shot 145384, 145747
+        configdict['config1'] = {}
+        cd = configdict['config1']
         chans = [i for i in range(1,9)]
         cols = [c for c in range(16,24)]
         rows = [34]*len(chans)]
@@ -104,13 +114,71 @@ class mappings:
         cols = [c for c in range(8,16)]
         setpos(cd,chans,rows,cols)
 
+        #config2, page 5 & 6 & 7 & 8, shot 171472, 171473, 171477, 171495
+        configdict['config2'] = {}
+        cd = configdict['config2']
+        chans = [i for i in range(17,25)]
+        cols = [c for c in range(16,24)]
+        rows = [33]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(25,33)]
+        rows = [32]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(33,41)]
+        rows = [31]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(41,49)]
+        rows = [30]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(49,57)]
+        rows = [29]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(57,65)]
+        rows = [28]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(1,9)]
+        rows = [27]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(9,17)]
+        rows = [26]*len(chans)]
+        setpos(cd,chans,rows,cols)
+
+        #config3, page 9, shot 176778
+        configdict['config3'] = {}
+        cd = configdict['config3']
+        chans = [i for i in range(1,9)]
+        cols = [c for c in range(16,24)]
+        rows = [35]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(9,17)]
+        rows = [34]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(17,25)]
+        rows = [33]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(25,33)]
+        rows = [32]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(33,41)]
+        rows = [31]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(41,49)]
+        rows = [30]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(49,57)]
+        rows = [29]*len(chans)]
+        setpos(cd,chans,rows,cols)
+        chans = [i for i in range(57,65)]
+        rows = [28]*len(chans)]
+        setpos(cd,chans,rows,cols)
+
         return self
 
     def setshotsepstate(self):
         '''
         Separatrix flag is 1 'inside', 2 'borderline', 3 'outside'
         '''
-        #sepstate0
+        #sepstate0, page 1, shot 142294
         sepdict['sepstate0'] = {}
         sd = sepdict['sepstate0']
         sd[32] = 1
@@ -140,7 +208,7 @@ class mappings:
         flags = [1]*2 + [2] + [3]*3
         setsep(sd,chans,flags)
 
-        #sepstate1
+        #sepstate1, page 2, shot 142300
         sepdict['sepstate1'] = {}
         sd = sepdict['sepstate1']
         sd[32] = 1
@@ -166,7 +234,7 @@ class mappings:
         flags = [3]*len(chans)
         setsep(sd,chans,flags)
 
-        #sepstate2
+        #sepstate2, page 3, shot 145384
         sepdict['sepstate2'] = {}
         sd = sepdict['sepstate2']
         chans = [i for i in range(1,9)]
@@ -188,7 +256,7 @@ class mappings:
         flags = [1]*len(chans)
         setsep(sd,chans,flags)
 
-        #sepstate3
+        #sepstate3, pg 4, shot 145747
         sepdict['sepstate3'] = {}
         sd = sepdict['sepstate3']
         chans = [i for i in range(1,9)]
@@ -211,6 +279,72 @@ class mappings:
         flags = [1]*len(chans)
         setsep(sd,chans,flags)
 
+        #sepstate4 pg 5 & 6, shot 171472, 171473
+        sepdict['sepstate4'] = {}
+        sd = sepdict['sepstate4']
+        chans = [i for i in range(17,25)]
+        flags = [1]*6 + [2] + [3]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(25,33)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(33,41)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(41,49)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(49,57)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(57,65)]
+        setsep(sd,chans,flags)
+        flags = [1]*7 + [3]
+        chans = [i for i in range(1,9)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(9,17)]
+        setsep(sd,chans,flags)
+
+        #sepstate5 pg 7 & 8, shot 171477, 171495
+        sepdict['sepstate5'] = {}
+        sd = sepdict['sepstate5']
+        chans = [i for i in range(17,25)]
+        flags = [1]*6 + [2] + [3]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(25,33)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(33,41)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(41,49)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(49,57)]
+        setsep(sd,chans,flags)
+        flags = [1]*7 + [3]
+        chans = [i for i in range(57,65)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(1,9)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(9,17)]
+        setsep(sd,chans,flags)
+
+        #sepstate6 pg 9, shot 176778
+        sepdict['sepstate6'] = {}
+        sd = sepdict['sepstate6']
+        chans = [i for i in range(1,9)]
+        flags = [1]*4 + [3]*4
+        setsep(sd,chans,flags)
+        chans = [i for i in range(9,17)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(17,25)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(25,33)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(33,41)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(57,65)]
+        setsep(sd,chans,flags)
+        flags = [1]*4 + [2] + [3]*3
+        chans = [i for i in range(41,49)]
+        setsep(sd,chans,flags)
+        chans = [i for i in range(49,57)]
+        setsep(sd,chans,flags)
+
         return self
 
 
@@ -221,51 +355,6 @@ def dealwithBESconfigs(shot = None):
     
     m = BESmap((64,64))
     m.setshotstring(shot)
-
-    '''
-    configdict = {
-            's142294': {
-                32: (0,31),
-                31: (8,31),
-                1:(),
-                5:(),
-                9:(),
-                13:(),
-                25:(),
-                41:(),
-                49:(),
-                57:(),
-            m.chanmap[0,31] = 32
-            m.chanmap[8,31] = 31
-            m.chanmap[16:24,31] = [1,5,9,13,25,41,49,57]
-            m.chanmap[16:24,30] = m.chanmap[16:24,31]+1
-            m.chanmap[16:24,29] = m.chanmap[16:24,31]+2
-            m.chanmap[16:24,28] = m.chanmap[16:24,31]+3
-            m.chanmap[20:24,27] = m.chanmap[20:24,31]+4
-            m.chanmap[20:24,26] = m.chanmap[20:24,31]+5
-            m.chanmap[20:24,25] = m.chanmap[20:24,31]+6
-            m.chanmap[20:24,24] = m.chanmap[20:24,31]+7
-            m.chanmap[16:20,27] = [17,19,21,23]
-            m.chanmap[16:20,26] = [18,20,22,24]
-            m.chanmap[16:21,25] = [33,34,35,36,37]
-            m.chanmap[16:21,24] = [0,0,38,39,40]
-
-            m.sepflags[0,31] = 32
-            m.sepflags[8,31] = 31
-            m.sepflags[16:24,31] = [1,5,9,13,25,41,49,57]
-            m.sepflags[16:24,30] = m.sepflags[16:24,31]+1
-            m.sepflags[16:24,29] = m.sepflags[16:24,31]+2
-            m.sepflags[16:24,28] = m.sepflags[16:24,31]+3
-            m.sepflags[20:24,27] = m.sepflags[20:24,31]+4
-            m.sepflags[20:24,26] = m.sepflags[20:24,31]+5
-            m.sepflags[20:24,25] = m.sepflags[20:24,31]+6
-            m.sepflags[20:24,24] = m.sepflags[20:24,31]+7
-            m.sepflags[16:20,27] = [17,19,21,23]
-            m.sepflags[16:20,26] = [18,20,22,24]
-            m.sepflags[16:21,25] = [33,34,35,36,37]
-            m.sepflags[16:21,24] = [0,0,38,39,40]
-        }
-    '''
 
     return m
             
