7356230eec0f6498f5acec19680735983f95fae8
Ryan Coffee <coffee@slac.stanford.edu>
2022-02-04 13:58:51 -0800

    certainly broken now... but simpler

--- src/analysis.py
+++ src/analysis.py
@@ -21,6 +21,11 @@ class Params:
         self.nfolds = {}
         self.dirthresh = {}
         self.chans = {}
+        self.chan = {'ece':-1,'bes':-1}
+        self.mask = {}
+        self.MASK = {}
+        self.dct_filt = {}
+        self.dct_deriv_filt = {}
 
     def setNsamples(self,s,n):
         self.nsamples[s]=n
@@ -78,21 +83,55 @@ class Params:
             ecegrp = f[detkey]
             eceorig = ecegrp.create_group('orig')
             ecelogabs = ecegrp.create_group('logabs')
-            self.setThresh
             ecedirectional = ecegrp.create_group('directional').attrs.create('threshold',1.e3*np.exp(-1.*np.power(np.arange(nsamples)/500.,int(2))) + 100.)
         if defkey == 'bes':
             besgrp = f[detkey]
+            besorig = besgrp.create_group('orig')
+            beslogabs = besgrp.create_group('logabs')
+            besdirectional = besgrp.create_group('directional').attrs.create('threshold',1.e3*np.exp(-1.*np.power(np.arange(nsamples)/500.,int(2))) + 100.)
+            bespop = besgrp.create_group('pop')
         return self
 
-    def initChans(self,det,data)
+    def initChans(self,det,data):
         self.chans[det] = list(data[det].keys())
+        self.chan[det] = -1
         return self
 
 
+    def initMasks(self,det):
+        self.mask[det],self.MASK[det] = utils.getderivmask3((self.nsamples[det],self.nfolds[det]))
+        return self
 
+    def buildFilt(self,det):
+        self.dct_filt[det] = utils.dct_buildfilt((2*self.nsamples[det],self.nfolds[det]),cut=(0,self.nsamples[det])) # remember, we need to mirror the nfolds dimension, thus the *2
+        if det == 'bes':
+            self.dct_deriv_filt[det] = utils.dct_deriv_buildfilt((2*self.nsamples[det],self.nfolds[det]),cut=(0,self.nsamples[det])) # remember, we need to mirror the nfolds dimension, thus the *2
+        return self
 
+    def goodChanKey(self,key,det):
+        m = re.search('^ece.{2}(\d+)$',chkey)
+        if not m:
+            self.chan[det] = -1
+            return False
+        self.chan[det] = np.uint8(m.group(1))
+        print('pid%i\t%s\t%s\t%ix%i'%(os.getpid(),m.group(0),self.chan[det],self.nsamples[det],self.nfolds[det]))
+        return True
 
+    def setOrig(self,f,det,d):
+        f[det]['orig'].create_dataset('%02i'%self.chan[detkey],data=d.astype(np.float16),dtype=np.float16)
+        return self
 
+    def setLogAbs(self,f,det,d):
+        f[det]['logabs'].create_dataset('%02i'%self.chan[det],data=d.astype(np.float16),dtype=np.float16)
+        return self
+
+    def setDirectional(self,f,det,d):
+        f[det]['directional'].create_dataset('%02i'%self.chan[det],data=d.astype(np.float16),dtype=np.float16)
+        return self
+
+    def setPop(self,f,det,d):
+        f[det]['pop'].create_dataset('%02i'%self.chan[det],data=d.astype(np.float16),dtype=np.float16)
+        return self
 
 def run_shot(params):
 
@@ -110,6 +149,7 @@ def run_shot(params):
     data['ece'] = np.load(ecefile,allow_pickle=True)
     data['bes'] = np.load(besfile,allow_pickle=True)
     dets = ['ece','bes']
+    params.initChans(data,dets)
     params.initTimes( data, dets ).setExtrema(dets)
     params.fillIndsCoince(dets).setFolds(dets)
     for det in dets:
@@ -119,36 +159,19 @@ def run_shot(params):
 
 
     with h5py.File(outfile,'w') as f:
-        ## working ECE first ##
+        #################### ECE section #########################3#
         detkey = 'ece'
+
         params.initH5det(f,detkey).fillH5times(f,detkey).setThresh(detkey)
         print('ECE nfolds*nsamples = %i * %i = %i'%(params.nfolds['ece'],params.nsamples['ece'],params.nfolds['ece']*params.nsamples['ece']))
-        params.initH5datasets(detkey)
+        params.initH5datasets(detkey).initMasks(detkey).buildFilt(detkey)
         ## threshold for ecedirectional max before zero crossing for frequencies th = 1e3*exp(-(x/500)**2)+100 where x is in index units as here.
-        '''
-        eceorig = ecegrp.create_group('orig')
-        ecelogabs = ecegrp.create_group('logabs')
-        ecedirectional = ecegrp.create_group('directional')
-        ecedirectional.attrs.create('threshold',1.e3*np.exp(-1.*np.power(np.arange(nsamples)/500.,int(2))) + 100.)
-        params.setthresh( 1.e3*np.exp(-1.*np.power(np.arange(params.getNsamples(detkey))/500.,int(2))) + 100. )
-        '''
 
-
-        HERE HERE HERE HERE
-
-        mask,MASK = utils.getderivmask3((nsamples,nfolds))
-
-        dct_filt = utils.dct_buildfilt((2*nsamples,nfolds),cut=(0,nsamples)) # remember, we need to mirror the nfolds dimension, thus the *2
-
-        for chkey in params.chans_ece:
-            m = re.search('^ece.{2}(\d+)$',chkey)
-            if m:
-                ch = np.uint8(m.group(1))
-                print('pid%i\t%s\t%s\t%ix%i'%(os.getpid(),m.group(0),ch,nsamples,nfolds))
-
-                if data_ece[chkey]['data.ECE'].shape[0]>1:
-                    x = data_ece[chkey]['data.ECE'][inds_ece_coince[0][:nsamples*nfolds]].reshape(nfolds,nsamples).T
-                    eceorig.create_dataset('%02i'%ch,data=x.astype(np.float16),dtype=np.float16)
+        for chkey in params.chans[detkey]:
+            if params.goodChanKey(chkey,detkey):
+                if data[detkey][chkey]['data.ECE'].shape[0]>1:
+                    x = data[detkey][chkey]['data.ECE'][params[detkey].inds_coince[0][:params.sz[detkey]]].reshape(params.nfolds[detkey],params.nsamples[detkey]).T
+                    params.setOrig[detkey](f,detkey,x) # will cast as np.float16
 
                     xx = np.row_stack((x,np.flip(x,axis=0)))
                     X = fft.dct(xx,axis=0)
@@ -157,79 +180,68 @@ def run_shot(params):
                     if np.max(AX)==0:
                         continue
                     OUT = np.log2(AX+1)
-                    OUT -= np.mean(OUT[-nsamples//4:,:])
+                    OUT -= np.mean(OUT[-params.nsamples[detkey]//4:,:])
 
                     qout = fft.dct(np.row_stack((OUT,np.flip(OUT,axis=0))),axis=0)
-                    FOUT = 2**8 * fft.idct(qout*dct_filt,axis=0)[:nsamples,:]
+                    FOUT = 2**8 * fft.idct(qout*params.dct_filt[detkey],axis=0)[:nsamples,:]
 
-                    ecelogabs.create_dataset('%02i'%ch,data=FOUT.astype(np.float16),dtype=np.float16)
+                    params.setLogAbs[detkey](f,detkey,FOUT)
+
 
                     DFTOUT = np.fft.fft2(FOUT/(2**8))
-                    NEWOUT = np.zeros((DFTOUT.shape[0],DFTOUT.shape[1],len(MASK)),dtype=float)
-                    for i in range(len(MASK)):
+                    NEWOUT = np.zeros((DFTOUT.shape[0],DFTOUT.shape[1],len(params.MASK[detkey])),dtype=float)
+                    for i in range(len(params.MASK[detkey])):
                         NEWOUT[:,:,i] = np.fft.ifft2(DFTOUT*MASK[i]).real * FOUT
-                    ecedirectional.create_dataset('%02i'%ch,data=NEWOUT.astype(np.float16),dtype=np.float16)
+                    params.setDirectional[detkey](f,detkey,NEWOUT[:params.nsamples[det],:,:])
 
 
 
-        besgrp = f.create_group('bes')
-        besgrp.attrs.create('sz',t_bes[inds_bes_coince].shape[0])
 
-        nsamples = params.nsamples_bes 
-        besgrp.attrs.create('nsamples',nsamples)
-        besgrp.attrs.create('nfolds',nfolds)
-        print('BES nfolds*nsamples = %i * %i = %i'%(nfolds,nsamples,nfolds*nsamples))
-        t = t_bes[inds_bes_coince[0][:nsamples*nfolds]].reshape(nfolds,nsamples).T
-        besgrp.create_dataset('times',data=t)
-        besorig = besgrp.create_group('orig')
-        beslogabs = besgrp.create_group('logabs')
-        #besdla = besgrp.create_group('derivlogabs')
-        besdirectional = besgrp.create_group('directional')
-        bespop = besgrp.create_group('pop')
-        #besrec = besgrp.create_group('rec')
+        ############### BES section ################
 
-        mask,MASK = utils.getderivmask3((nsamples,nfolds))
+        detkey = 'bes'
 
-        #dct_filt_ELMrecover = utils.dct_deriv_buildfilt((nsamples*2,nfolds),cut=(0,nsamples//4)) 
-        dct_filt_ELMpop = utils.dct_deriv_buildfilt((nsamples*2,nfolds),cut=(0,nsamples)) 
+        params.initH5det(f,detkey).fillH5times(f,detkey).setThresh(detkey)
+        print('BES nfolds*nsamples = %i * %i = %i'%(params.nfolds['bes'],params.nsamples['bes'],params.nfolds['bes']*params.nsamples['bes']))
+        params.initH5datasets(detkey).initMasks(detkey).buildFilt(detkey)
+        ## threshold for ecedirectional max before zero crossing for frequencies th = 1e3*exp(-(x/500)**2)+100 where x is in index units as here.
 
-        dct_filt_bes = utils.dct_buildfilt((nsamples*2,nfolds),cut=(0,nsamples)) 
-        #dct_filt_besdderiv = utils.dct_dderiv_buildfilt((nsamples*2,nfolds),cut=(0,nsamples)) 
 
-        for chkey in chans_bes:
-            m = re.search('^bes.{2}(\d+)',chkey)
-            if m:
-                ch = np.uint8(m.group(1))
-                print('pid%i\t%s\t%s\t%ix%i'%(os.getpid(),m.group(0),m.group(1),nsamples,nfolds))
-                if (data_bes[chkey]['data.BES'].shape[0]>1):
-                    x = data_bes[chkey]['data.BES'][inds_bes_coince[0][:nsamples*nfolds]].reshape(nfolds,nsamples).T
-                    besorig.create_dataset('%02i'%ch,data=x.astype(np.float16),dtype=np.float16)
+        print('BES nfolds*nsamples = %i * %i = %i'%(params.nfolds[detkey],params.nsamples[detkey],params.sz[detkey]))
+
+        for chkey in params.chans[detkey]:
+            if params.goodChanKey(chkey,detkey):
+                if data[detkey][chkey]['data.BES'].shape[0]>1:
+                    x = data[detkey][chkey]['data.BES'][params[detkey].inds_coince[0][:params.sz[detkey]]].reshape(params.nfolds[detkey],params.nsamples[detkey]).T
+                    params.setOrig[detkey](f,detkey,x) # will cast as np.float16
 
                     xx = np.row_stack((x,np.flip(x,axis=0)))
                     X = fft.dct(xx,axis=0)
 
-                    elmpop = -fft.idst(X*dct_filt_ELMpop,axis=0) 
+                    ##### ONLY UNIQUE SECTION ########
+                    elmpop = -fft.idst(X*params.dct_deriv_filt[detkey],axis=0) 
                     elmpop -= np.mean(elmpop)
-                    bespop.create_dataset('%02i'%ch,data=elmpop[:nsamples,:].astype(np.float16),dtype=np.float16)
+                    params.setPop[detkey](f,detkey,elmpop[:params.nsamples[detkey],:])
+                    ##################################
 
-
-                    AX = np.abs(X)[::2,:] 
+                    X[0,:] = 0 ####################### this also was only in ECE, but adding to BES for consistency... let's see.
+                    AX = np.abs(X[::2,:])
                     if np.max(AX)==0:
                         continue
                     OUT = np.log2(AX+1)
-                    OUT -= np.mean(OUT[-nsamples//4:,:])
+                    OUT -= np.mean(OUT[-params.nsamples[detkey]//4:,:])
+
                     qout = fft.dct(np.row_stack((OUT,np.flip(OUT,axis=0))),axis=0)
-                    FOUT = 2**8 * fft.idct(qout*dct_filt_bes,axis=0)[:nsamples,:]
-                    beslogabs.create_dataset('%02i'%ch,data=FOUT.astype(np.float16),dtype=np.float16)
+                    FOUT = 2**8 * fft.idct(qout*params.dct_filt[detkey],axis=0)[:nsamples,:]
 
-                    DFTOUT = np.fft.fft2(FOUT/2**8)
-                    NEWOUT = np.zeros((DFTOUT.shape[0],DFTOUT.shape[1],len(MASK)),dtype=float)
-                    for i in range(len(MASK)):
-                        NEWOUT[:,:,i] = np.fft.ifft2(DFTOUT*MASK[i]).real * FOUT
-                    besdirectional.create_dataset('%02i'%ch,data=NEWOUT[:nsamples,:,:].astype(np.float16),dtype=np.float16)
+                    params.setLogAbs[detkey](f,detkey,FOUT)
 
 
-
+                    DFTOUT = np.fft.fft2(FOUT/(2**8))
+                    NEWOUT = np.zeros((DFTOUT.shape[0],DFTOUT.shape[1],len(params.MASK[detkey])),dtype=float)
+                    for i in range(len(params.MASK[detkey])):
+                        NEWOUT[:,:,i] = np.fft.ifft2(DFTOUT*params.MASK[detkey][i]).real * FOUT
+                    params.setDirectional[detkey](f,detkey,NEWOUT[:params.nsamples[detkey],:,:])
 
         #closing with h5py.File() as f
     return
