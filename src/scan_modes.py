4d632e9a1d8cf4b699e7236da9cbff40c0871930
Ryan Coffee <coffeer76@gmail.com>
2022-02-17 11:42:32 -0800

    incremental

--- src/scan_modes.py
+++ src/scan_modes.py
@@ -4,6 +4,7 @@ import h5py
 import numpy as np
 import utils
 import bitstring as bs
+import re
 
 import argparse
 parser = argparse.ArgumentParser(description='Parallel scanner for finding spectrogram modes from .h5 files\nmain -opath <opath> -nthreads <nthreads> -infiles <.h5 filename separated by sapce>')
@@ -13,23 +14,39 @@ parser.add_argument('-infiles',   type=str, nargs='+',required=True, help='list 
 
 def main():
     args, unparsed = parser.parse_known_args()
-    expand = 1
+    expand = -8
     for fname in args.infiles:
         with h5py.File(fname,'r') as f:
-            d = f['ece']['directional']['05'][()]
-            binfmtstr = '#0%ib'%(d.shape[0]*expand)
+            chan = 5
+            d = f['ece']['directional']['%02i'%chan][()]
+            binfmtstr = '#08b'
+            if expand >0:
+                binfmtstr = '#0%ib'%(d.shape[0]*expand+4)
+            else:
+                binfmtstr = '#0%ib'%(d.shape[0]//abs(expand)+4)
+            print(binfmtstr)
             sparseout = []
             barrays = {}
             for v in range(d.shape[2]): # v for directional view
                 barrays[v] = []
                 for s in range(d.shape[1]): # s for time step
                     result = 0b0
-                    edges = np.unique( utils.scanedges(d[:,s,v],thresh=300,expand=expand) ) # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
+                    if expand > 0:
+                        edges = np.unique( utils.scanedges(d[:,s,v],thresh=300,expand=expand) ) # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
+                    else:
+                        tmp = utils.scanedges(d[:,s,v],thresh=300,expand=1) # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
+                        edges = np.unique( [int(v/abs(expand)) for v in tmp] )
+
                     for e in edges:
                         result += 2**int(e)
                         sparseout.append([int(e),s,v])
                     barrays[v] += [format(result,binfmtstr)]
-                    print(barrays[v][-1])
+                m = re.search('^.*/(\d+_\w+).h5$',fname)
+                if m:
+                    print(m.group(1))
+                    ofname = '%s/%s.c%02i.v%i.dat'%(args.opath,m.group(1),chan,v)
+                    for s in range(len(barrays[v])):
+                        print(barrays[v][s])
             
 
     return
