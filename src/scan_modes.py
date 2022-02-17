5440fd9f3a7e361fa8647f516f0279084914de5d
Ryan Coffee <coffeer76@gmail.com>
2022-02-16 22:42:41 -0800

    start using bitarrays

--- src/scan_modes.py
+++ src/scan_modes.py
@@ -3,6 +3,7 @@
 import h5py
 import numpy as np
 import utils
+import bitstring as bs
 
 import argparse
 parser = argparse.ArgumentParser(description='Parallel scanner for finding spectrogram modes from .h5 files\nmain -opath <opath> -nthreads <nthreads> -infiles <.h5 filename separated by sapce>')
@@ -12,14 +13,23 @@ parser.add_argument('-infiles',   type=str, nargs='+',required=True, help='list 
 
 def main():
     args, unparsed = parser.parse_known_args()
+    expand = 1
     for fname in args.infiles:
         with h5py.File(fname,'r') as f:
             d = f['ece']['directional']['05'][()]
+            binfmtstr = '#0%ib'%(d.shape[0]*expand)
             sparseout = []
+            barrays = {}
             for v in range(d.shape[2]): # v for directional view
+                barrays[v] = []
                 for s in range(d.shape[1]): # s for time step
-                    for e in utils.scanedges(d[:,s,v],thresh=300,expand=4): # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
+                    result = 0b0
+                    edges = np.unique( utils.scanedges(d[:,s,v],thresh=300,expand=expand) ) # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
+                    for e in edges:
+                        result += 2**int(e)
                         sparseout.append([int(e),s,v])
+                    barrays[v] += [format(result,binfmtstr)]
+                    print(barrays[v][-1])
             
 
     return
