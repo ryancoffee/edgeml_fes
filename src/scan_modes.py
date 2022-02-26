b3ea813bc6ef05f4bc6ee24348876500509bea61
Ryan Coffee <coffeer76@gmail.com>
2022-02-25 22:31:28 -0800

    encode/decode

--- src/scan_modes.py
+++ src/scan_modes.py
@@ -6,6 +6,7 @@ import utils
 import bitstring as bs
 import re
 import os
+import gmpy2
 
 import argparse
 parser = argparse.ArgumentParser(description='Parallel scanner for finding spectrogram modes from .h5 files\nmain -opath <opath> -nthreads <nthreads> -infiles <.h5 filename separated by sapce>')
@@ -16,43 +17,49 @@ parser.add_argument('-expand', type=int, default=1,required=False,help='exand hi
 parser.add_argument('-contract', type=int, default=1,required=False,help='contract hist resolution (1=no change), e.g. res = unique(int(edges//contract))')
 parser.add_argument('-infiles',   type=str, nargs='+',required=True, help='list of .h5 filenames')
 
-def encode(edges):
-    s = 0b0
+def encode(edges,sz):
+    s = gmpy2.xmpz()
     for e in edges:
         s += 2**int(e)
-    return s
+    nwords = sz//64
+    wordlist = []
+    for w in range(nwords):
+        wordlist += [np.uint64(s%(2**64))]
+        s = s >> 64
+    return wordlist
+
+def bindist(x,y):
+    d = gmpy2.xmpz(x^y)
+    return len( list(d.iter_set()) )
 
 def decode(s):
-    l = []
+    l = gmpy2.xmpz()
+    for i,v in enumerate(s):
+        l += gmpy2.xmpz(v) << (i*64)
     return l
-
-'''
-reserved for use in decode section
-                            binfmtstr = '08b'
-                            if expand >0:
-                                binfmtstr = '0%ib'%(d.shape[0]*expand+4)
-                            else:
-                                binfmtstr = '0%ib'%(d.shape[0]//abs(expand)+4)
-'''
 
 def addsentences(story,d,args):
     chanlist = list(d.keys())
     (nfreqs,nsteps,nviews) = d[chanlist[0]][()].shape
-    sentence = []
+    sz = np.uint16(nfreqs)
     for step in range(nsteps):
+        print('step:\t%i'%step)
+        paragraph = story.create_group('paragraph_%06i'%step) #### Somehow need to use h5py.opaque_dtype() I think
         for chan in chanlist: ## for every sentence build the words
-            word = []
+            sentences = []
             for v in range(nviews): # for every word build the character string
                 if args.expand >= 1:
                     edges = np.unique( utils.scanedges( d[chan][()][:,step,v] ,thresh=args.thresh,expand=args.expand) )
-                else:
+                    sz *= args.expand
+                elif args.contract > 1:
                     tmp = utils.scanedges( d[chan][()][:,step,v] ,thresh=args.tresh,expand=1) 
                     # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
                     edges = np.unique( [int(val/args.contract) for val in tmp] )
-                word += [ encode( edges ) ] 
-            sentence += [ np.array(word) ]
+                    sz /= args.contract
+                words = encode( edges , sz)  
+                sentences += [words]
             ## now I have a sentence for every timestep
-            story.create_dataset('sentence_%06i'%step,data=np.stack(sentence,axis=1),dtype='B') #### Somehow need to use h5py.opaque_dtype() I think
+            paragraph.create_dataset('%s'%chan,data=np.stack(sentences,axis=-1),dtype=np.uint64) #### Somehow need to use h5py.opaque_dtype() I think
     return story
 
 
@@ -68,18 +75,8 @@ def main():
                     for det in ['ece','bes']:
                         of.create_group(det)
                         story = of[det].create_group('directional')
-                        '''
-                        OK, now for each detector (each timestep), there will be 4 directional vectors (views) of hist, 
-                        each view encoded as a binary that is N-bits long, where N is the number of discrete "frequencies" 
-                        in the spectrogram, such that each "view" is a "character"
-                        The list of encoded "characters" along the direction dimension makes a "word" for each channel.
-                        The list of "words" for all channels make a "sentence" that gets added to our "story" of time-steps.
-
-                        First get the number of timesteps using channel 0
-                        '''
                         d = f[det]['directional']
                         story = addsentences(story,d,args)
-
     return
 
 if __name__ == '__main__':
