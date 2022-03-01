c06e3a6592f3ea53e0579bb116d785c19b5aa020
Ryan Coffee <coffeer76@gmail.com>
2022-03-01 10:36:44 -0800

    added encodings

--- src/scan_modes.py
+++ src/scan_modes.py
@@ -3,10 +3,11 @@
 import h5py
 import numpy as np
 import utils
+import encodings
 import bitstring as bs
 import re
 import os
-import gmpy2
+import multiprocessing as mp
 
 import argparse
 parser = argparse.ArgumentParser(description='Parallel scanner for finding spectrogram modes from .h5 files\nmain -opath <opath> -nthreads <nthreads> -infiles <.h5 filename separated by sapce>')
@@ -17,46 +18,29 @@ parser.add_argument('-expand', type=int, default=1,required=False,help='exand hi
 parser.add_argument('-contract', type=int, default=1,required=False,help='contract hist resolution (1=no change), e.g. res = unique(int(edges//contract))')
 parser.add_argument('-infiles',   type=str, nargs='+',required=True, help='list of .h5 filenames')
 
-def encode(edges,sz):
-    s = gmpy2.xmpz()
-    for e in edges:
-        s += 2**int(e)
-    nwords = sz//64
-    wordlist = []
-    for w in range(nwords):
-        wordlist += [np.uint64(s%(2**64))]
-        s = s >> 64
-    return wordlist
-
-def bindist(x,y):
-    d = gmpy2.xmpz(x^y)
-    return len( list(d.iter_set()) )
-
-def decode(s):
-    l = gmpy2.xmpz()
-    for i,v in enumerate(s):
-        l += gmpy2.xmpz(v) << (i*64)
-    return l
 
 def addsentences(story,d,args):
     chanlist = list(d.keys())
     (nfreqs,nsteps,nviews) = d[chanlist[0]][()].shape
-    sz = np.uint16(nfreqs)
+    if args.contract > 1:
+        sz = np.uint16( max(64 , nfreqs // args.contract) )
+    else:
+        sz = np.uint16(nfreqs*args.expand)
+    print(sz)
     for step in range(nsteps):
         print('step:\t%i'%step)
-        paragraph = story.create_group('paragraph_%06i'%step) #### Somehow need to use h5py.opaque_dtype() I think
+        paragraph = story.create_group('step_%06i'%step) #### Somehow need to use h5py.opaque_dtype() I think
         for chan in chanlist: ## for every sentence build the words
             sentences = []
             for v in range(nviews): # for every word build the character string
-                if args.expand >= 1:
-                    edges = np.unique( utils.scanedges( d[chan][()][:,step,v] ,thresh=args.thresh,expand=args.expand) )
-                    sz *= args.expand
-                elif args.contract > 1:
-                    tmp = utils.scanedges( d[chan][()][:,step,v] ,thresh=args.tresh,expand=1) 
+                if args.contract > 1:
+                    tmp = utils.scanedges( d[chan][()][:,step,v] ,thresh=args.thresh,expand=1) 
                     # expand = 4, for ece this means edges run 0:2048 rather than original 0:512... bes would go to 4096
                     edges = np.unique( [int(val/args.contract) for val in tmp] )
-                    sz /= args.contract
-                words = encode( edges , sz)  
+                else:
+                    edges = np.unique( utils.scanedges( d[chan][()][:,step,v] ,thresh=args.thresh,expand=args.expand) )
+                words = encodings.encode( edges , sz=sz)  
+                #print(len(words))
                 sentences += [words]
             ## now I have a sentence for every timestep
             paragraph.create_dataset('%s'%chan,data=np.stack(sentences,axis=-1),dtype=np.uint64) #### Somehow need to use h5py.opaque_dtype() I think
