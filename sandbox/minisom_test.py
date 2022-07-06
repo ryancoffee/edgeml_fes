a8abf9a975d66757a03698a640ad0215b4372145
Ryan Coffee <coffeer76@gmail.com>
2022-07-05 18:48:18 -0700

    sandboxing self organizing maps

--- sandbox/minisom_test.py
+++ sandbox/minisom_test.py
@@ -5,6 +5,15 @@ import h5py
 import math
 import sys
 
+'''
+@misc{vettigliminisom,
+  title={MiniSom: minimalistic and NumPy-based implementation of the Self Organizing Map},
+  author={Giuseppe Vettigli},
+  year={2018},
+  url={https://github.com/JustGlowing/minisom/},
+}
+'''
+
 def erf(x,xc,w):
     if (x<(xc-w)): return 0.
     if (x>(xc+w)): return 1.
@@ -32,6 +41,7 @@ def sampledist(clusters,nbins,nsamples,darkcounts):
     return xvals
 
 def main():
+    rng = np.random.default_rng()
     bits = int(8)
     nbins = int(1)<<bits
     print('Starting with nbins = %i'%(nbins))
@@ -47,13 +57,28 @@ def main():
     clusters = {'one':[233,10,190,5],'two':[120,30,60,40],'three':[40,50,150,25],'four':[128,64,128,64]}
     xvals = sampledist(clusters,nbins,nsamples,darkcounts)
     dims = list(xvals.keys())
+
     h,bx,by = np.histogram2d(xvals[dims[0]],xvals[dims[1]],bins=(np.arange(int(1)<<bits)))
-    '''
-    for i in range(len(xvals['oneone'])):
-        print(xvals['oneone'][i],xvals['onetwo'][i])
-    '''
     np.savetxt('tmphist2d.dat',h,fmt='%i')
     
+    
+    clusters = {'one':[233,10,190,5,33,20,180,50],'two':[120,30,60,40,220,20,200,10],'three':[40,50,150,25,60,50,128,32],'four':[128,64,128,64,128,64,128,64]}
+    xvals = sampledist(clusters,nbins,nsamples,darkcounts)
+    dims = list(xvals.keys())
+    print(dims)
+    data = [ [xvals[d][s] for d in dims] for s in range(len(xvals[dims[0]])) ]
+    print(len(data))
+
+    som = minisom.MiniSom(16,16,len(dims),sigma=8,learning_rate=.5)
+    som.train(data,3000)
+    inds = np.arange(len(xvals[dims[0]]))
+    rng.shuffle(inds)
+
+    with open('tmpout.dat','w') as f:
+        _= [print('%i\t%i\t%i'%(i,som.winner(data[i])[0],som.winner(data[i])[1]),file=f) for i in inds]
+
+    print(som.get_weights().shape)
+    _=[ np.savetxt('som_weights_%i.dat'%i,som.get_weights()[:,:,i],fmt='%.2f') for i in range(som.get_weights().shape[-1])]
 
     return
 
