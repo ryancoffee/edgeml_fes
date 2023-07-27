f742bdac0cb221c6c4167a00f18f6a1566d34741
Ryan Coffee <coffee@slac.stanford.edu>
2023-07-27 16:53:47 -0700

    incremental

--- src/quantizeECE.py
+++ src/quantizeECE.py
@@ -29,46 +29,51 @@ def main(nbins,fnames):
     for fname in validfnames:
         #print(fname)
         flist += [h5py.File(fname,'r')]
+        m = re.search('.*(\d+)_.*\.h5',fname)
 
     if len(flist)==0:
         print('no files worked')
         return
 
     chankeys = list(flist[0]['ece']['edges']['locations'].keys())
+    nframes = []
     for f in flist:
+        nframes += [f['ece']['edges']['nedges']['00'].shape[0]]
         for k in chankeys:
             allhits += list(f['ece']['edges']['locations'][k][()])
 
     quant.setbins(allhits)
 
     if plotting:
-        #plt.plot(quant.bincenters(),1000.0/(0.01 + quant.binwidths()))
-        #plt.plot(quant.bincenters(),quant.histogram(allhits))
-        plt.stairs(10000.0/(0.01 + quant.binwidths()),quant.binedges())
-        plt.stairs(quant.histogram(allhits),quant.binedges())
+        plt.plot(quant.bincenters(),1000.0/(0.01 + quant.binwidths()))
+        plt.plot(quant.bincenters(),quant.histogram(allhits))
+        #plt.stairs(10000.0/(0.01 + quant.binwidths()),quant.binedges())
+        #plt.stairs(quant.histogram(allhits),quant.binedges())
         plt.show()
 
-    with h5py.File('./data.h5','w') as o:
-        for j,f in enumerate(flist):
-            data = np.zeros((len(chankeys),quant.nbins,10000))
-            for c,k in enumerate(chankeys):
-                ne = f['ece']['edges']['nedges'][k][()]
-                for i in range(len(ne)):
-                    a = np.sum(ne[:i])
-                    data[c,:,i] += quant.histogram(f['ece']['edges']['locations'][k][()][a:a+ne[i]])
-            shot = re.search('\/\d+_fft.h5',validfnames[j])
-            print(shot)
-            o.create_dataset(shot,data=data)
+    for j,f in enumerate(flist):
+        data = np.zeros((len(chankeys),quant.nbins,nframes[j]),dtype=np.uint8)
+        for c,k in enumerate(chankeys):
+            ne = f['ece']['edges']['nedges'][k][()]
+            a = 0
+            for i in range(len(ne)):
+                data[c,:,i] += quant.histogram(f['ece']['edges']['locations'][k][()][a:a+ne[i]]).astype(np.uint8)
+                a += ne[i]
+        shot = (re.search('.*\/(\d+)_.*\.h5',validfnames[j])).group(1)
+        inds = np.where(data>0)
+        print(inds)
+        print(data[inds])
+        print(np.max(data))
+        print(data.shape)
+        print('Writing shot_%s'%shot)
+        with h5py.File('../quantized_data.h5','a') as o:
+            o.create_dataset('shot_%s'%shot,data=data)
+            o.create_dataset('shot_%s_qbins'%shot,data=quant.binedges())
             if plotting:
-                plt.pcolor(np.sum(data[10:13,:,:],axis=0))
+                plt.pcolor(data[35,:,:])
                 plt.colorbar()
                 plt.clim(0,2)
                 plt.show()
-            inds = np.where(data>0)
-            print(inds)
-            print(data[inds])
-            print(np.max(data))
-            print(data.shape)
 
 
     _=[f.close() for f in flist]
