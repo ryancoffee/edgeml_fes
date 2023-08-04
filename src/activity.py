85e9e30ce33788459173e007e02294338851aa4a
Ryan Coffee <coffee@slac.stanford.edu>
2023-08-03 18:29:55 -0700

    shotslist

--- src/activity.py
+++ src/activity.py
@@ -4,6 +4,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import sys
+import re
 
 def process(fname,outmat):
     with h5py.File(fname) as f:
@@ -15,11 +16,26 @@ def process(fname,outmat):
     return outmat
 
 def main(fname):
+    m = re.search('.*/(\d+)_fft.h5',fname)
+    if m:
+        shotnum = m.group(1)
+    else:
+        shotnum = 'bad shotname'
     outmat = np.zeros((1,),dtype=np.uint8)
     outmat = process(fname,outmat)
     plt.pcolor(outmat)
     plt.clim(0,1<<4)
-    plt.colorbar()
+    #fig,axs = plt.subplots(2)
+    #axs[0].pcolor(outmat)
+    #outfft = np.fft.fft2(outmat)
+    #filt = np.cos(np.arange(outfft.shape[0])*np.pi*2/float(outfft.shape[0])) + 1
+    #filt2d = np.tile(np.expand_dims(filt,axis=-1),outfft.shape[1])
+    #filtx = np.cos(np.arange(outfft.shape[1])*np.pi*2./float(outfft.shape[1])) + 1
+    #filt2d *= np.tile(np.expand_dims(filtx,axis=-1),outfft.shape[0]).T
+    #res = np.fft.ifft2(outfft*filt2d).real
+    #axs[1].pcolor(res)
+    #axs[1].colorbar()
+    plt.title(shotnum)
     plt.show()
 
 if __name__ == '__main__':
